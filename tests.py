af48b398863bc7c2431ba08eca2323927b3dbbf9
Jimmy Kamau <jimmy.kamau@andela.com>
2016-08-08 10:47:06 +0300

    change Person class initialization to only include person's name

--- tests.py
+++ tests.py
@@ -22,7 +22,7 @@ class TestClasses(unittest.TestCase):
 		cls._office = Office([])
 		cls._room = Room(4, "Oculus", "office")
 		cls._fellow = Fellow()
-		cls._person = Person("12345", "John Doe")
+		cls._person = Person("John Doe")
 		cls._staff = Staff()
 
 	def test_class_initialization(self):
