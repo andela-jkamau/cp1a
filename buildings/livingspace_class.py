5616044b71e70e921aec1b20f48090981bc88fa0
Jimmy Kamau <jimmy.kamau@andela.com>
2016-08-09 18:13:57 +0300

    implement **kwargs in class definitions

--- buildings/livingspace_class.py
+++ buildings/livingspace_class.py
@@ -8,6 +8,12 @@ class LivingSpace(Room):
 		people_allocated = A list of the identifiers of the people allocated to a room. 
 	"""
 	
-	def __init__(self, people_allocated):
-		super().__init__(rooms)
+	def __init__(self, people_allocated=[], **kwargs):
+		super().__init__(**kwargs)
 		self.people_allocated = people_allocated
+
+	def add_person_to_room(self, person_name):
+		if len(self.people_allocated) < self.room_capacity:
+			self.people_allocated.append(person_name)
+		else:
+			return "ROOM FULL"
