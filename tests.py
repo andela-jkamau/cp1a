6f1ed18a0402d7fc8f4607d933e5f8a93c6ef11f
Jimmy Kamau <jimmy.kamau@andela.com>
2016-09-10 18:44:04 +0300

    add tests

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 import unittest
-from subprocess import getoutput
-
+from unittest import mock
 import buildings.room_functions as room_functions
 import humans.person_functions as person_functions
 
@@ -11,66 +10,141 @@ class TestClasses(unittest.TestCase):
             Tests for program's continuous development.
     """
 
-    """@classmethod
-    def setUpClass(cls):  # Set up required classes
-        cls._amity = Amity()
-        cls._livingspace = LivingSpace(
-            room_capacity=4, room_name="Bash", room_type="living space")
-        cls._office = Office(
-            room_capacity=4, room_name="Hogwarts", room_type="office")
-        cls._room = Room(4, "Oculus", "office")
-        cls._fellow = Fellow(name="John Doe")
-        cls._person = Person("John Doe")
-        cls._staff = Staff(name="John Doe")
+    def test_1_person_creation_with_no_rooms(self):
 
-    def test_class_initialization(self):
-        self.assertIsInstance(
-            self._amity, Amity,
-            msg="Cannot create `Amity` instance")
-        self.assertIsInstance(
-            self._livingspace, LivingSpace,
-            msg="Cannot create `LivingSpace` instance")
-        self.assertIsInstance(
-            self._office, Office,
-            msg="Cannot create `Office` instance")
-        self.assertIsInstance(
-            self._room, Room,
-            msg="Cannot create `Room` instance")
-        self.assertIsInstance(
-            self._fellow, Fellow,
-            msg="Cannot create `Fellow` instance")
-        self.assertIsInstance(
-            self._person, Person,
-            msg="Cannot create `Person` instance")
-        self.assertIsInstance(
-            self._staff, Staff,
-            msg="Cannot create `Staff` instance")"""
+        # Test creation of person with empty office
+        person_args = {
+            "<person_first_name>": "Jimmy",
+            "<person_other_name>": "Kamau",
+            "<person_type>": "Fellow",
+            "<wants_accommodation>": "N"
+        }
+        person_functions.create_person(person_args)
 
-    """def test_commands_callability(self):
-        # Test help message
-        self.assertEqual(getoutput(["./amity.py -h"]), 'Welcome to Amity. Amity helps you allocate rooms to people at random.\nUsage:\n    amity create_room <room_name>...\n    amity add_person <person_name> <FELLOW|STAFF> [wants_accommodation]\n    amity reallocate_person <person_identifier> <new_room_name>\n    amity load_people [-f=filename]\n    amity print_allocations [-o=filename]\n    amity print_unallocated [-o=filename]\n    amity print_room <room_name>\n    amity save_state [--db=sqlite_database]\n    amity load_state <sqlite_database>\n    amity (-i | --interactive)\n    amity (-h | --help)\nOptions:\n    -i, --interactive  Interactive Mode\n    -h, --help  Show this screen and exit.')
+        self.assertEqual(
+            person_functions.people[1].name,
+            "Jimmy Kamau",
+            msg="Person being created with wrong name"
+        )
+        self.assertEqual(
+            person_functions.people[1].office_allocated,
+            None,
+            msg="Person should have no office if none has been created"
+        )
+        self.assertFalse(
+            person_functions.people[1].livingspace_allocated,
+            msg="Person should have no living space if none has been created"
+        )
 
-        # Test creation of rooms
-        self.assertEqual(getoutput(["./amity.py create_room Hogwarts Valhalla"],
-                                   'The rooms Hogwarts Valhalla have been created successfully'))
+    @mock.patch('builtins.input', side_effect=["O"])
+    def test_2_office_creation(self, input):
 
-        # Test addition of a fellow that doesn't require a living space
-        self.assertEqual(getoutput(["./amity.py add_person John Doe FELLOW"],
-                                   'Fellow John Doe has been added to the system. An office has been allocated to them'))
+        # Test creation of office
+        office_args = {
+            "<room_name>": ["Valhalla"]
+        }
+        room_functions.create_room(office_args)
 
-        # Test addition of a Fellow that requires a living space
-        self.assertEqual(getoutput(["./amity.py add_person Jimmy Kamau FELLOW Y"],
-                                   'Fellow Jimmy Kamau has been added to the system. An office and living space has been allocated to them'))
+        self.assertEqual(
+            len(room_functions.current_rooms.rooms),
+            1,
+            msg="Cannot create a new room"
+        )
+        self.assertTrue(
+            "Valhalla" in
+            room_functions.current_rooms.available_offices,
+            msg="Empty room not being added to list of available rooms"
+        )
 
-        # Test addition of a member of Staff
-        self.assertEqual(getoutput(["./amity.py add_person Jane Doe STAFF"],
-                                   'Staff member Jane Doe has been added to the system. An office has been allocated to them'))
+    @mock.patch('builtins.input', side_effect=["L"])
+    def test_3_living_space_creation(self, input):
 
-        # Test addition of a member of Staff with living space
-        self.assertEqual(getoutput(["./amity.py add_person Jane Doe STAFF Y"],
-                                   'ERROR: A living space cannot be allocated to a member of Staff'))
+        # Test creation of living space
+        living_space_args = {
+            "<room_name>": ["Bash"]
+        }
+        room_functions.create_room(living_space_args)
 
-        # Test person reallocation"""
+        self.assertEqual(
+            len(room_functions.current_rooms.rooms),
+            2,
+            msg="Cannot create more than one room"
+        )
+        self.assertTrue(
+            "Bash" in
+            room_functions.current_rooms.available_livingspaces,
+            msg="Empty living space not being added to list of available living spaces"
+        )
+
+    def test_4_person_creation_with_rooms(self):
+
+        # Test creation of fellow with office and living space
+        person_args = {
+            "<person_first_name>": "John",
+            "<person_other_name>": "Doe",
+            "<person_type>": "Fellow",
+            "<wants_accommodation>": "Y"
+        }
+        person_functions.create_person(person_args)
+
+        self.assertEqual(
+            person_functions.people[2].name,
+            "John Doe",
+            msg="Person being created with wrong name"
+        )
+        self.assertEqual(
+            person_functions.people[2].office_allocated,
+            "Valhalla",
+            msg="Person not being allocated to an office"
+        )
+        self.assertEqual(
+            person_functions.people[2].livingspace_allocated,
+            "Bash",
+            msg="Person not being allocated to a living space"
+        )
+
+        # Test creation of fellow with office and no living space
+        person_args = {
+            "<person_first_name>": "Jane",
+            "<person_other_name>": "Doe",
+            "<person_type>": "Fellow",
+            "<wants_accommodation>": "N"
+        }
+        person_functions.create_person(person_args)
+
+        self.assertEqual(
+            person_functions.people[3].name,
+            "Jane Doe",
+            msg="Person being created with wrong name"
+        )
+        self.assertEqual(
+            person_functions.people[3].office_allocated,
+            "Valhalla",
+            msg="Person not being allocated to an office"
+        )
+        self.assertFalse(
+            person_functions.people[3].livingspace_allocated,
+            msg="Person being allocated to a living space when they don't want accommodation"
+        )
+
+        # Test creation of staff
+        person_args = {
+            "<person_first_name>": "Brian",
+            "<person_other_name>": "Ngure",
+            "<person_type>": "Staff",
+            "<wants_accommodation>": False
+        }
+        person_functions.create_person(person_args)
+
+        self.assertEqual(
+            person_functions.people[4].office_allocated,
+            "Valhalla",
+            msg="Person not being allocated to an office"
+        )
+        self.assertRaises(
+            AttributeError,
+            lambda: person_functions.people[4].livingspace_allocated,
+        )
 
 
 if __name__ == '__main__':
