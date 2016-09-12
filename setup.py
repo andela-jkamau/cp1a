f0bb35e6c50d92a9acd9483e959e77deb052ab36
Jimmy Kamau <jimmy.kamau@andela.com>
2016-09-12 13:42:16 +0300

    update setup with relevant information

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 setup(
-    name='cp1A',
-    description='Checkpoint 1A',
+    name='Amity',
+    description='This system will be used to'
+    ' automatically allocate spaces to people in Amity at random.',
     author='Jimmy Kamau',
     author_email='jimmy.kamau@andela.com',
     url='https://github.com/andela-jkamau/cp1a',
